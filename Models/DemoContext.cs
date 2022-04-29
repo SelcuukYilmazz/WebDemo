16ba7264d338dc54a3b0069b1879dafd2dad8741
Selçuk Yılmaz <selcukyilmaz2000@hotmail.com>
2022-04-29 06:08:48 +0300

    Add project files.

---
+++ Models/DemoContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebDemo.Models
+{
+    public class DemoContext : DbContext
+    {
+        public DbSet<UserModel> Users { get; set;}
+        protected override void OnConfiguring(DbContextOptionsBuilder options) => options.UseSqlite(@"Data Source=C:\SQL-VERİTABANI\QuizWeb.db");
+    }
+}
---
+++ Models/UserModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebDemo.Models
+{
+    public class UserModel
+    {
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = " Please Enter Name")]
+        public String Name { get; set; }
+
+        [Required (ErrorMessage = " Please Enter Password")]
+        public String Password { get; set; }
+        public bool Sign_status { get; set; }
+    }
+}
